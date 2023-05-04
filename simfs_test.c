e967ad0b936ab87e119da8375abe3eca94e0c7d8
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-05-04 14:51:43 -0700

    test image_open fail and test image_close fail: passed

--- simfs_test.c
+++ simfs_test.c
@@ -14,17 +14,30 @@ void test_non_existent_image_open_and_close(void) {
 }
 
 void test_existing_image_open_and_close(void) {
-    image_fd = image_open("exisiting_test.txt", 0);
-    CTEST_ASSERT(image_fd != -1, "Test opening existenting file");
-    CTEST_ASSERT(image_close() != -1, "Test closing exisiting file");
+    image_fd = image_open("existing_test.txt", 0);
+    CTEST_ASSERT(image_fd != -1, "Test opening existing file");
+    CTEST_ASSERT(image_close() != -1, "Test closing existing file");
+}
+
+void test_image_open_fail(void) {
+    image_fd = image_open(NULL, 0);
+    CTEST_ASSERT(image_fd == -1, "Test failure of image_open");
+}
+
+void test_image_close_fail(void) {
+    image_fd = 123456789;
+    CTEST_ASSERT(image_close() == -1, "Test failure of image_close");
 }
 
 
 int main(void) {
     CTEST_VERBOSE(1);
 
+    // image.c - image_open(), image_close()
     test_non_existent_image_open_and_close();
     test_existing_image_open_and_close();
+    test_image_open_fail();
+    test_image_close_fail();
 
     CTEST_RESULTS();
 
