c78c23f10c2f8984df806b51cc38b0dd739de086
MatthewDyerOSU <dyerma@oregonstate.edu>
2023-05-04 14:25:20 -0700

    test exisiting image open and close: passed

--- simfs_test.c
+++ simfs_test.c
@@ -10,6 +10,13 @@ void test_non_existent_image_open_and_close(void) {
     image_fd = image_open("test.txt", 0);
     CTEST_ASSERT(image_fd != -1, "Test opening non-existent file");
     CTEST_ASSERT(image_close() != -1, "Test closing newly made file");
+    remove("test.txt");
+}
+
+void test_existing_image_open_and_close(void) {
+    image_fd = image_open("exisiting_test.txt", 0);
+    CTEST_ASSERT(image_fd != -1, "Test opening existenting file");
+    CTEST_ASSERT(image_close() != -1, "Test closing exisiting file");
 }
 
 
@@ -17,6 +24,7 @@ int main(void) {
     CTEST_VERBOSE(1);
 
     test_non_existent_image_open_and_close();
+    test_existing_image_open_and_close();
 
     CTEST_RESULTS();
 
